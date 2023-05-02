1f93dd697014375a7670f8db46d48b24943eb813
NatEiEi <97091093+NatEiEi@users.noreply.github.com>
2023-05-02 23:50:25 +0700

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,31 +1,34 @@
 public class Tester {
     public static void main(String[] args) {
-        Landlord L = new Landlord("null", "null", "null", "Name", "null", 123, 456);
+        Landlord L = new Landlord("aaa1", "abc123", "aaa1@gmail.com", "First", "Last", 076554512, 45632326);
 
-        Dormitory D = new Dormitory(0, null, 0, null
-                        , null, null, null, null, null);
+        Dormitory D = new Dormitory(01, "helloworld", 123, "bangkok"
+                        , null, null, null, null, "123");
 
-        Room r1 = new Room(1, 2, 3 , D);
-        Room r2 = new Room(4, 5, 6 , D);
+        Room r1 = new Room(01, 22, 2000, D);
+        Room r2 = new Room(02, 50, 5000 , D);
 
         L.addRoom(r1);
         L.addRoom(r2);
 
+        System.out.println("Show all landlord's room");
         L.showMyRoom();
 
-        System.out.println("-----------------------");
+        System.out.println("\nShow detail room");
         r1.showRoom();
+        System.out.println("\nShow name landlord");
         r1.showLandlord();
+        System.out.println("\nShow room Address");
         r1.showAddress();
         
         
-        System.out.println("Room status : " + r1.rentAble());
+        System.out.println("\nRoom status : " + r1.rentAble());
 
         Tenant T = new Tenant(null, null, null, null, null, 0, null, 0);
         T.rent(r1);
-        System.out.println("Room status : " + r1.rentAble());
+        System.out.println("\nRoom status : " + r1.rentAble());
         T.showMyRoom();
         T.cancel();
-        System.out.println("Room status : " + r1.rentAble());
+        System.out.println("\nRoom status : " + r1.rentAble());
     }
 }
